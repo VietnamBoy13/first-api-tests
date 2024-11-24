5efad53dfc1deb39252fc3e500ad725d531fd1db
VietnamBoy13 <alter.tom@inbox.com>
2024-11-25 03:42:41 +0400

    выполнил задание по практике

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,3 +8,15 @@ class TestID:
         id_find_animal = test_case.find_animal(id_new_animal)['id']
         assert id_new_animal == id_find_animal, (
             "[FAILED]: Ids doesnt match {} and {}".format(id_new_animal, id_find_animal))
+
+    def test_delete(self):
+        test_case = TestCase()
+        id_new_animal = test_case.create_animal('Cat')
+        id_find_animal = test_case.find_animal(id_new_animal)['id']
+        assert id_new_animal == id_find_animal, (
+            "[FAILED]: Ids doesnt match {} and {}".format(id_new_animal, id_find_animal))
+
+        id_removed_animal = test_case.remove_animal(id_new_animal)
+        message_find_animal = test_case.find_animal(id_new_animal)['message']
+        assert id_new_animal == id_find_animal, (
+            "[FAILED]: Ids doesnt match {} and {}".format(id_new_animal, id_find_animal))
